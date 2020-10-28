5a7e4e258f207dc2cc31af6b70024296720c05f9
pyqpyqpyq <alwayswannaflyx@gmail.com>
2020-10-28 17:51:06 +0800

    [pyq] complete the basic function without decorations

--- src/Mainpage/Mainpage.js
+++ src/Mainpage/Mainpage.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-alert */
+
 /* eslint-disable react/no-unused-state */
 /* eslint-disable react/button-has-type */
 import React, { Component } from 'react';
-
-// import { postAPI } from '../API/api';
-// eslint-disable-next-line import/no-unresolved
-import './Mainpage.css';
+import "./Mainpage.css";
 
 class Mainpage extends Component {
   // eslint-disable-next-line react/state-in-constructor
@@ -13,52 +11,8 @@ class Mainpage extends Component {
     addingTeamMemberName: '',
     isAddTeamMember: false,
     teamvisible: false,
-    teamlist: [
-      // [
-      //   { id: '1', name: '成吉思汗' },
-      //   { id: '2', name: '鲁班七号' },
-      //   { id: '3', name: '太乙真人' },
-      // ],
-      // [
-      //   { id: '4', name: '钟无艳' },
-      //   { id: '5', name: '花木兰' },
-      //   { id: '6', name: '雅典娜' },
-      // ],
-      // [
-      //   { id: '7', name: '芈月' },
-      //   { id: '8', name: '白起' },
-      //   { id: '9', name: '刘禅' },
-      // ],
-      // [
-      //   { id: '10', name: '庄周' },
-      //   { id: '11', name: '马超' },
-      // ],
-      // [
-      //   { id: '12', name: '刘备' },
-      //   { id: '13', name: '哪吒' },
-      // ],
-      // [
-      //   { id: '14', name: '大乔' },
-      //   { id: '15', name: '蔡文姬' },
-      // ],
-    ],
-    teamMembers: [
-      { id: '1', name: '成吉思汗' },
-      { id: '2', name: '鲁班七号' },
-      { id: '3', name: '太乙真人' },
-      { id: '4', name: '钟无艳' },
-      { id: '5', name: '花木兰' },
-      { id: '6', name: '雅典娜' },
-      { id: '7', name: '芈月' },
-      { id: '8', name: '白起' },
-      { id: '9', name: '刘禅' },
-      { id: '10', name: '庄周' },
-      { id: '11', name: '马超' },
-      { id: '12', name: '刘备' },
-      { id: '13', name: '哪吒' },
-      { id: '14', name: '大乔' },
-      { id: '15', name: '蔡文姬' },
-    ],
+    teamlist: [],
+    teamMembers: [],
   };
 
   componentDidMount() {
@@ -66,7 +20,7 @@ class Mainpage extends Component {
   }
 
   getAllTeamMembers = () => {
-    fetch('http://localhost:8080/team')
+    fetch('http://localhost:8080/members')
       .then((response) => response.json())
       .then((data) =>
         this.setState({
@@ -82,7 +36,7 @@ class Mainpage extends Component {
   };
 
   handleDivideTeam = () => {
-    const url = 'http://localhost:8080/groups';
+    const url = 'http://localhost:8080/teams';
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
@@ -90,6 +44,7 @@ class Mainpage extends Component {
           teamlist: data,
         });
       });
+    // console.log(1);
   };
 
   handleNameChange = (event) => {
@@ -100,7 +55,7 @@ class Mainpage extends Component {
 
   addNewTeamMember = (event) => {
     if (event.keyCode === 13) {
-      const url = 'http://localhost:8080/teammember';
+      const url = 'http://localhost:8080/members';
       fetch(url, {
         mode: 'cors',
         header: {
@@ -135,7 +90,7 @@ class Mainpage extends Component {
           <span>{team.name}</span>
         </header>
         <section>
-          {team.teamlist.map((teammember) => (
+          {team.memberDtoList.map((teammember) => (
             <button className="teamMemberbutton" key={teammember.id}>
               {teammember.id}.{teammember.name}
             </button>
@@ -148,7 +103,7 @@ class Mainpage extends Component {
       <div className="page">
         <div className="content">
           <h4>分组列表</h4>
-          <button className="clickbutton" onClick={this.handleDivideTeam()}>
+          <button className="clickbutton" onClick={this.handleDivideTeam}>
             分组学员
           </button>
         </div>
